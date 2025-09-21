4bf50161345cb39f8bf1f1b9095ec3431e1fa39b
Anton Popov <panton295@gmail.com>
2025-09-21 13:44:03 +0900

    Tweak blink.cmp config

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -3,17 +3,15 @@ return {
         'saghen/blink.cmp',
         version = '1.*',
         opts = {
-            keymap = {
-                preset = 'enter',
-                ['<Down>'] = { function(cmp) cmp.select_next({ auto_insert = false }) end },
-                ['<Up>'] = { function(cmp) cmp.select_prev({ auto_insert = false }) end },
-                ['<C-n>'] = { function(cmp) cmp.select_next({ auto_insert = false }) end },
-                ['<C-p>'] = { function(cmp) cmp.select_prev({ auto_insert = false }) end },
+            keymap = { preset = 'enter' },
+            completion = {
+                list = { selection = { preselect = true, auto_insert = false } };
+                ghost_text = { enabled = false },
+                documentation = { auto_show = false },
             },
+            sources = { default = { 'lsp', 'path', 'snippets' }, },
+            fuzzy = { implementation = 'prefer_rust_with_warning' },
             appearance = { nerd_font_variant = 'mono' },
-            completion = { documentation = { auto_show = false } },
-            sources = { default = { 'lsp', 'path', 'snippets', 'buffer' }, },
-            fuzzy = { implementation = 'prefer_rust_with_warning' }
         },
         opts_extend = { 'sources.default' }
     }
