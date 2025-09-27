dc1e9126d9b01b17f0c6968975cfc0afa0e05623
Anton Popov <panton295@gmail.com>
2025-09-28 00:33:36 +0900

    Remove normal mode from yank to OS clipboard mapping

--- dot_config/nvim/lua/config/mappings.lua
+++ dot_config/nvim/lua/config/mappings.lua
@@ -10,7 +10,7 @@ vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<Nop>')
 
 -- OS clipboard mappings
-vim.keymap.set({ 'v', 'n' }, '<Leader>y', '\"+y') -- Copy to OS clipboard in visual mode
+vim.keymap.set({ 'v' }, '<Leader>y', '\"+y') -- Copy to OS clipboard in visual mode
 vim.keymap.set({ 'n' }, '<Leader>yy', '\"+yy')    -- Copy to OS clipboard an entire line in normal mode
 vim.keymap.set({ 'v', 'n' }, '<Leader>p', '\"+p') -- Paste from OS clipboard after the cursor
 vim.keymap.set({ 'v', 'n' }, '<Leader>P', '\"+P') -- Paste from OS clipboard before the cursor
