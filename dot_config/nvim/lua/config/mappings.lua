744be8040b672410f0f4f4a7458ac28605b01be6
Anton Popov <panton295@gmail.com>
2025-09-29 21:32:21 +0900

    Clean up split panes mappings

--- dot_config/nvim/lua/config/mappings.lua
+++ dot_config/nvim/lua/config/mappings.lua
@@ -3,6 +3,9 @@
 -- Disable some keys before doing everything else
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<nop>') -- Disable 'Space' key
 
+-- Map CTRL+c to Escape
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-c>', '<Esc>')
+
 -- Disable arrow keys
 vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', '<Nop>')
@@ -10,7 +13,7 @@ vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<Nop>')
 vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<Nop>')
 
 -- OS clipboard mappings
-vim.keymap.set({ 'v' }, '<Leader>y', '\"+y') -- Copy to OS clipboard in visual mode
+vim.keymap.set({ 'v' }, '<Leader>y', '\"+y')      -- Copy to OS clipboard in visual mode
 vim.keymap.set({ 'n' }, '<Leader>yy', '\"+yy')    -- Copy to OS clipboard an entire line in normal mode
 vim.keymap.set({ 'v', 'n' }, '<Leader>p', '\"+p') -- Paste from OS clipboard after the cursor
 vim.keymap.set({ 'v', 'n' }, '<Leader>P', '\"+P') -- Paste from OS clipboard before the cursor
@@ -21,28 +24,25 @@ vim.keymap.set({ 'v' }, '<Leader>P', '"_dP') -- Delete without putting content i
 -- Disable search highlighting
 vim.keymap.set({ 'n' }, '<Leader>/', ':nohlsearch<CR>')
 
--- Panes and tabs mappings
-vim.keymap.set({ 'n' }, '<Leader>sv', ':vsplit<CR>') -- Open vertical split pane
-vim.keymap.set({ 'n' }, '<Leader>sh', ':split<CR>')  -- Open horizontal split pane
-vim.keymap.set({ 'n' }, '<Leader>t', ':tabe<CR>')    -- Open a new tab
-vim.keymap.set({ 'n' }, '<Leader>q', ':q<CR>')       -- Close a buffer
+-- Split panes and tabs mappings
+vim.keymap.set({ 'n' }, '<Leader>wv', ':vsplit<CR>') -- Create a new vertical split
+vim.keymap.set({ 'n' }, '<Leader>wh', ':split<CR>')  -- Create a new horizontal split
+vim.keymap.set({ 'n' }, '<Leader>tn', ':tabe<CR>')   -- Create a new tab
+vim.keymap.set({ 'n' }, '<Leader>q', ':q<CR>')       -- Close current active buffer
 
 -- Split panes navigation mappings <Leader> shortcuts
+vim.keymap.set({ 'n' }, '<Leader>r', '<C-w>r') -- Rotate split panes
 vim.keymap.set({ 'n' }, '<Leader>j', '<C-w>j')
 vim.keymap.set({ 'n' }, '<Leader>k', '<C-w>k')
 vim.keymap.set({ 'n' }, '<Leader>h', '<C-w>h')
 vim.keymap.set({ 'n' }, '<Leader>l', '<C-w>l')
-vim.keymap.set({ 'n' }, '<Leader>r', '<C-w>r') -- Rotate split panes
 
--- Resize panes mappings <Leader> shortcuts
-vim.keymap.set({ 'n' }, '<Leader>=', '<C-w>=')
-vim.keymap.set({ 'n' }, '<M-=>', '3<C-w>+')
-vim.keymap.set({ 'n' }, '<M-->', '3<C-w>-')
-vim.keymap.set({ 'n' }, '<M-,>', '2<C-w><')
-vim.keymap.set({ 'n' }, '<M-.>', '2<C-w>>')
-
--- Map CTRL+c to Escape
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-c>', '<Esc>')
+-- Split panes resizing
+vim.keymap.set({ 'n' }, '<Leader>we', '<C-w>=') -- Equalize splits' sizes
+vim.keymap.set({ 'n' }, '<M-t>', '<C-w>5+')
+vim.keymap.set({ 'n' }, '<M-s>', '<C-w>5-')
+vim.keymap.set({ 'n' }, '<M-w>', '<C-w>5>')
+vim.keymap.set({ 'n' }, '<M-n>', '<C-w>5<')
 
 -- Place cursor at the center of the screen while scrolling
 vim.keymap.set({ 'n' }, '<C-d>', '<C-d>zz')
@@ -55,7 +55,6 @@ vim.keymap.set({ 'n' }, 'n', 'nzz')
 vim.keymap.set({ 'n' }, 'N', 'Nzz')
 
 -- Lsp mappings
-
 -- Format an entire buffer
 vim.keymap.set({ 'n' }, '<Leader>fc', vim.lsp.buf.format)
 -- Disable Omni completion mapping because we use blink.cmp
